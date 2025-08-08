79e56da464b679cda8ab398221568cd2211e8d5b
Mohammad Elias <meprogramer2024@gmail.com>
2025-08-08 14:27:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const rec = new SpeechRecognition();
+
+rec.language = "en-US";
+rec.continuous = false;
+
+rec.onresult = function (e) {
+  const acceptedColors = [
+    "red",
+    "green",
+    "blue",
+    "yellow",
+    "pink",
+    "brown",
+    "purple",
+    "orange",
+    "black",
+    "white",
+    "gray",
+    "grey",
+  ];
+
+  for (let i = e.resultIndex; i < e.results.length; i++) {
+    const script = e.results[i][0].transcript.toLowerCase().trim();
+
+    if (acceptedColors.includes(script)) {
+        document.body.style.backgroundColor = script;
+    } else {
+        alert('Please say a color');
+    }
+  }
+};
+
+rec.start();
